d5ffe24a1ce451d7adbabd7714b95be523b765e5
Anthony DeChiaro <taintedkernel@gmail.com>
2015-06-08 17:50:18 -0700

    Bugfixes & better error checking

--- vim/setup-vim.sh
+++ vim/setup-vim.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Check to see if destination folders/files exist
 if [ -f "$HOME/.vimrc" ]; then
     echo "$HOME/.vimrc exists, aborting"
     exit 1
@@ -10,15 +11,34 @@ if [ -d "$HOME/.vim" ]; then
     exit 1
 fi
 
+# Make sure we're in the correct source directory
+cd `dirname $0`
+
+# Check to see if source folders/files exist
+if [ ! -f ".vimrc" ]; then
+    echo "Source file .vimrc missing, aborting"
+    exit 1
+fi
+
+if [ ! -d ".vim" ]; then
+    echo "Source folder .vim missing, aborting"
+    exit 1
+fi
+
+# Link .vimrc
 SRC=${PWD#$HOME/}
-echo -n "Linking files to \$HOME/$SRC ..."
-cd
+echo -n "Linking files to $HOME/$SRC ..."
 echo -n " .vimrc"
-ln -s $SRC/.vimrc
-echo -n " .vim/"
-ln -s $SRC/.vim
+ln -s $SRC/.vimrc $HOME/.vimrc
 echo
 
+# Copy .vim
+echo -n "Copying files to $HOME ..."
+echo -n " .vim"
+cp -a .vim $HOME
+echo
+
+# Install vim-debug
 echo "Installing vim-debug..."
 pip install --user vim-debug
 
